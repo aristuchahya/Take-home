6ca64b0eb67538dc6d419bcc4e957adff3f87b26
aristu09 <namikazeuzumaki43@gmail.com>
2024-08-25 10:38:08 +0700

    optimalize-backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [CommonModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
