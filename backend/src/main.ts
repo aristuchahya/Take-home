0574836d3d88385b76689376290896594b82d934
aristu09 <namikazeuzumaki43@gmail.com>
2024-10-11 20:38:35 +0700

    deployment1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
