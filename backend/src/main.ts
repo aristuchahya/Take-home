57ef65a54d418f28d08fadf31d75fdd14a9e9113
aristu09 <namikazeuzumaki43@gmail.com>
2024-10-13 08:47:07 +0700

    deploy8

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
+let server: Server;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   app.enableCors();
   app.setGlobalPrefix('api/v1');
 
@@ -11,4 +20,32 @@ async function bootstrap() {
     console.log(`Server is running on port ${port}`);
   });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrapServer());
+  return server;
+};
+
+const bootstrapServer = async (): Promise<Server> => {
+  if (!server) {
+    const expressApp = express();
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    app.enableCors();
+    app.setGlobalPrefix('api/v1');
+
+    await app.init();
+    server = expressApp.listen(3000);
+  }
+  return server;
+};
+
+if (process.env.VERCEL !== '1') {
+  bootstrap();
+}
